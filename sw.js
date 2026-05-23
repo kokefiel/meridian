efa5ef56915495c08674aeb3ef706c807a362a91
kokefiel <jbernabeubatalla@gmail.com>
2026-05-23 10:14:49 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,21 @@
-const C='ritmo-v1';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html'])))});
-self.addEventListener('activate',e=>self.clients.claim());
+const C='meridian-v2';
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html'])));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
 self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    return caches.open(C).then(c=>{c.put(e.request,resp.clone());return resp;});
-  }).catch(()=>caches.match('./index.html'))));
+  // network first, fallback to cache
+  e.respondWith(
+    fetch(e.request).then(resp=>{
+      const clone=resp.clone();
+      caches.open(C).then(c=>c.put(e.request,clone));
+      return resp;
+    }).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html')))
+  );
 });
